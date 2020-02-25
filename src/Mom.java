ef87200d73e67a77966c3164b8a984c27d055885
yourUsername <yourEmail@domain.com>
2020-02-24 22:05:14 -0500

    dfs

---
+++ src/Mom.java
@@ -0,0 +1,43 @@
+public class Mom {
+public static void main(String[]args) {
+
+   //== is equal to
+    //<= less than or equal to
+    //> greater than
+    //>= greater than or equal to
+    //!= is not equal to
+
+    int a = 5;
+   int b = 6;
+// when we write the boolean value the computer read it either as a true or a false value)
+String l = "deb";
+String d = "tr";
+   char jj = 's';
+   char ll = 'g';
+    boolean c =(a!=b);
+   System.out.println(c);
+    boolean k = (a>b);
+  System.out.println(k);
+    boolean j = (a<=b);
+  System.out.println( j );
+
+    //boolean h = (l==k)
+    System.out.println( l );
+  // System.out.println( j );
+
+   double kg = 2.4;
+// if the statement is false the s.o.p does not print only do when the value is false
+   if(a != 2.4){
+       System.out.println( "They are equal!" );
+   }
+
+   if(a > 7){
+       System.out.println( "Greater than 7" );
+   }
+
+   if(kg!=2.4 || kg >=7);
+   System.out.println( " a is 2.4 or 2.5 ");
+
+}
+
+}
